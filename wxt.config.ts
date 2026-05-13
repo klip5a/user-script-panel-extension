1369a5b64e4093beef7819c448b86d7c79daea56
klip5a <klip5a.dev@gmail.com>
2026-05-13 12:46:42 +0500

    Add extension icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -9,8 +9,22 @@ export default defineConfig({
     description: "Панель инструментов администратора для CNC1/Bitrix",
     permissions: ["storage", "activeTab", "scripting", "sidePanel"],
     host_permissions: ["https://cnc1.ru/*", "https://www.cnc1.ru/*"],
+    icons: {
+      16: "icon/16.png",
+      32: "icon/32.png",
+      48: "icon/48.png",
+      96: "icon/96.png",
+      128: "icon/128.png"
+    },
     action: {
-      default_title: "CNC1 UserPanel"
+      default_title: "CNC1 UserPanel",
+      default_icon: {
+        16: "icon/16.png",
+        32: "icon/32.png",
+        48: "icon/48.png",
+        96: "icon/96.png",
+        128: "icon/128.png"
+      }
     },
     side_panel: {
       default_path: "sidepanel.html"
