e89692d085d496650edc9273e20830c22602ba34
klip5a <klip5a.dev@gmail.com>
2026-07-09 21:59:25 +0500

    Add section filter search helper

--- src/features/site-enhancements/index.ts
+++ src/features/site-enhancements/index.ts
@@ -2,6 +2,7 @@ export { sortHighlight, useSortHighlight } from "./sort-highlight";
 export { SelectHelper, selectHelper } from "./select-helper";
 export { propertySorter } from "./property-sorter";
 export { sectionSorter } from "./section-sorter";
+export { sectionFilterSearch } from "./section-filter-search";
 export { productMassEditor } from "./product-mass-editor";
 export { filterSortCheck } from "./filter-sort-check";
 export { imageInfoHighlight } from "./image-info-highlight";
